7c0e5124693770f3b633236104807af9911b19a1
Jens Segers <segers.jens@gmail.com>
2014-03-22 15:14:18 +0100

    Css tweaks

--- app/views/ask.blade.php
+++ app/views/ask.blade.php
@@ -51,7 +51,7 @@
       </div>
       <div class="form-group">
         <div class="col-sm-offset-2 col-sm-10">
-          <button type="submit" class="btn btn-default">Stel vraag</button>
+          <button type="submit" class="btn btn-primary">Stel vraag</button>
         </div>
       </div>
     </form>
