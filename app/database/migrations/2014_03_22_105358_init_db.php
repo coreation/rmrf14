48054c0e1dd976b22618545212dd862294ae61aa
Jan Vansteenlandt <vansteenlandt.jan@gmail.com>
2014-03-22 13:18:05 +0100

    Question repository

--- app/database/migrations/2014_03_22_105358_init_db.php
+++ app/database/migrations/2014_03_22_105358_init_db.php
@@ -6,30 +6,30 @@ use Illuminate\Database\Migrations\Migration;
 class InitDb extends Migration
 {
 
-	/**
-	 * Run the migrations.
-	 *
-	 * @return void
-	 */
-	public function up()
-	{
-		// Create feedback database
-		Schema::create('feedback', function($table){
-			$table->increments('id');
-			$table->string('identifier', 255);
-			$table->text('feedback');
-			$table->timestamps();
-		});
-	}
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // Create feedback database
+        Schema::create('question', function($table){
+            $table->increments('id');
+            $table->string('question', 255);
+            $table->text('votes');
+            $table->timestamps();
+        });
+    }
 
-	/**
-	 * Reverse the migrations.
-	 *
-	 * @return void
-	 */
-	public function down()
-	{
-		// Drop feedback database
-		Schema::drop('feedback');
-	}
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        // Drop feedback database
+        Schema::drop('question');
+    }
 }
